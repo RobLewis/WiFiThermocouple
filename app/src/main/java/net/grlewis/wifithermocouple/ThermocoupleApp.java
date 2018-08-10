d2b77d5e512d3a61a75244a5fbaa0948ea98e6ce
Rob Lewis <groblewis@mac.com>
2018-08-09 19:09:29 -0700

    add Application class

---
+++ app/src/main/java/net/grlewis/wifithermocouple/ThermocoupleApp.java
@@ -0,0 +1,10 @@
+package net.grlewis.wifithermocouple;
+
+import android.app.Application;
+
+public class ThermocoupleApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate( );
+    }
+}
