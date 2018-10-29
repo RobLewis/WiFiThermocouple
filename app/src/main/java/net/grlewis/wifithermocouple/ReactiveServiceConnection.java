93c301a42a27ee8c6bedcb28b0ef521277c2bfb2
jack@calderadev.com <groblewis@mac.com>
2018-10-28 17:11:44 -0700

    bit more work on ReactiveServiceConnection

--- app/src/main/java/net/grlewis/wifithermocouple/ReactiveServiceConnection.java
+++ app/src/main/java/net/grlewis/wifithermocouple/ReactiveServiceConnection.java
@@ -8,27 +8,58 @@ import com.jakewharton.rxrelay2.BehaviorRelay;
 
 import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 /*
-* subscribe to bind, dispose to unbind
-*
-* emits service reference
-* completes if service goes away?
-* errors if service can't be bound
-*
-* */
+ * subscribe to bind, dispose to unbind
+ *
+ * emits service reference
+ * completes if service goes away?
+ * errors if service can't be bound
+ * unbinds if disposed
+ *
+ * */
 
-class ReactiveServiceConnection<Service> implements ServiceConnection, ObservableSource<Service> {
+class ReactiveServiceConnection<S> implements ServiceConnection, ObservableSource<S> {  // S is the Service type
+    // note ObservableSource calls the Observer's onSubscribe(), onNext(), onError(), and onComplete() methods
+    
+    private Observer subscriber;
+    private RSCDisposable disposable;
+    private boolean disposed;
+    
+    private S serviceRef;
     
     
-    Observer subscriber;
-    
-    
-    @Override  // respond by calling observer.onSubscribe( Disposable d )
-    public void subscribe( Observer<? super Service> observer ) {
-        subscriber = observer;
-        /////////
+    private class RSCDisposable implements Disposable {  // has methods .dispose() and .isDisposed()
+        
+        @Override // if called, unbind Service
+        public void dispose( ) {
+            disposed = true;
+            ////////
+        }
+        
+        @Override
+        public boolean isDisposed( ) {
+            return disposed;
+        }
     }
+    
+    
+    // constructor
+    public ReactiveServiceConnection( ) {
+        this.disposable = new RSCDisposable();
+    }
+    
+    
+    @Override
+    // respond by calling observer.onSubscribe( Disposable d )
+    public void subscribe( Observer<? super S> observer ) {
+        subscriber = observer;
+        observer.onSubscribe( disposable );
+        ///////// bind service & emit connection
+    }
+    
+    
     
     @Override
     public void onServiceConnected( ComponentName name, IBinder service ) {
